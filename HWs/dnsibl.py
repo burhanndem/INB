04dc17ba8574827a3d9a20b6aa2c8c0af72e6a8a
burhanndem <burhanndemirtas@gmail.com>
2018-08-09 15:57:20 +0300

    okurken gözün acımasın diye edit

--- HWs/dnsibl.py
+++ HWs/dnsibl.py
@@ -30,25 +30,16 @@ class Mhs():
             if col2 != []:
                 for i in range(0, len(col2), 1):
                     col2text.append(col2[i].text)
-            # col3 = soup.find("ul",{"class": "list"})
-            # if col3 != []:
-            #     for i in range(0, len(col3), 1):
-            #         col3text.append(col3.text)
 
-
-        # pprint(col1text[0].split('\n'))
-        ### pprint(col1text[0].split('\n'))
         x = PrettyTable()
         x.field_names = basliklar           #tablonun sütunlarını basliklar listemin elemanları olarak tanımlıyorum
-        #
+
         x.add_column(x.field_names[0], col1text[0].split()+col1text[1].split())    #datanın içinde bolca \n var onlardan kurtulup kolonun içine atıyorum
-        # x.add_column(x.field_names[1], col2text[0].split())
-        # x.add_column(x.field_names[2], col3text[0].split('\n'))
 
-        # for title, lst in zip(basliklar, itertools.zip_longest(col1text, col2text, fillvalue="")):
+        x.add_column(x.field_names[1], col2text[0].split())
+
+
+        # for title, lst in zip(basliklar, itertools.zip_longest(col1text, fillvalue="")):      #zip_longest denemeleri başarısız
         #     x.add_column(title, lst)
+
         print(x.get_string(title=f"{site} Sitesinin Bilgileri"))
-
-        # pprint(col1text[0].split())
-
-
